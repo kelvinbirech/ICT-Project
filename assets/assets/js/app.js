3f117f5ba4a4eb223aef3542b9fecc0872e77581
kelvinbirech <62088569+kelvinbirech@users.noreply.github.com>
2021-07-09 03:41:11 -0700

    Delete assets/assets directory

--- assets/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-
-const menuBtn = document.querySelector(".menu");
-const navigation = document.querySelector(".navlinks");
-
-menuBtn.addEventListener("click" ,() => {
-menuBtn.classList.toggle("active");
-navigation.classList.toggle("active");
-});
