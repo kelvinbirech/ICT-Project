e5e877e374ac160aa8fc68ee870acd1786aebea1
Humphrey Kamau <58790731+Grayhkamau@users.noreply.github.com>
2021-08-15 16:19:48 +0300

    Add files via upload

--- migrations.php
+++ migrations.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once './core/database.php';
+    require_once '/xampp/htdocs/Linkme/search/database.php';
     //users table
 
     echo "Creating users table\n";
@@ -12,30 +12,35 @@
         `password` tinytext NOT NULL,
         `added_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
-    $STH = $DBH->prepare($sql);
+    $STH = $db->prepare($sql);
     $STH->execute();
     echo "Created users table\n";
 
     //albums table
     echo "Creating profile table\n";
 
-    $STH = $DBH->prepare("CREATE TABLE IF NOT EXISTS `profiles` (
-        `id` SERIAL,
-        
-        `displayname` tinytext NOT NULL,
-        `expertise` tinytext NOT NULL,
-        `about` tinytext NOT NULL,
-        `website` tinytext,
-        `email` tinytext NOT NULL,
-        `telephone` tinytext NOT NULL,
-        `user` bigint(20) UNSIGNED NOT NULL,
-        `added_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-        FOREIGN KEY(user) REFERENCES user(id) ON DELETE RESTRICT ON UPDATE CASCADE
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4");
-    $STH->execute();
-    echo "Created profile table\n";
+$STH = $db->prepare("CREATE TABLE IF NOT EXISTS `profiles` (
+    `id` SERIAL,
 
+    `displayname` tinytext NOT NULL,
+    `expertise` tinytext NOT NULL,
+    `about` tinytext NOT NULL,
+    `image` tinytext,
+    `website` tinytext,
+    `email` tinytext NOT NULL,
+    `telephone` tinytext NOT NULL,
+    `user` bigint(20) UNSIGNED NOT NULL,
+    `added_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    FOREIGN KEY(user) REFERENCES user(id) ON DELETE RESTRICT ON UPDATE CASCADE
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4");
+$STH->execute();
+echo "Created profile table\n";
 
-    echo "finished apllying all migrations."
+// $STH = $db->prepare("ALTER TABLE profiles ADD salary INT");
+// $STH->execute();
+$STH = $db->prepare("ALTER TABLE profiles ADD experience INT(100)");
+$STH->execute();
+
+echo "finished applying all migrations."
 
  ?>
