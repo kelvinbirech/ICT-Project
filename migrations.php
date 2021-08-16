2d42092e37119e555f4d09d4b5d25a40d323fc4a
vybekid1 <kiplangatedwin734@gmail.com>
2021-08-16 13:33:16 +0300

    Edited Search Pages And Update To README

--- migrations.php
+++ migrations.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once './search/database.php';
+    require_once './core/database.php';
     //users table
 
     echo "Creating users table\n";
@@ -12,14 +12,14 @@
         `password` tinytext NOT NULL,
         `added_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
-    $STH = $db->prepare($sql);
+    $STH = $DBH->prepare($sql);
     $STH->execute();
     echo "Created users table\n";
 
     //albums table
     echo "Creating profile table\n";
 
-$STH = $db->prepare("CREATE TABLE IF NOT EXISTS `profiles` (
+$STH = $DBH->prepare("CREATE TABLE IF NOT EXISTS `profiles` (
     `id` SERIAL,
 
     `displayname` tinytext NOT NULL,
@@ -38,7 +38,7 @@ echo "Created profile table\n";
 
 // $STH = $db->prepare("ALTER TABLE profiles ADD salary INT");
 // $STH->execute();
-$STH = $db->prepare("ALTER TABLE profiles ADD experience INT(100)");
+$STH = $DBH->prepare("ALTER TABLE profiles ADD experience INT(100)");
 $STH->execute();
 
 echo "finished applying all migrations."
